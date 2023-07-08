e853f87e5096089fbde567c9b22eec04f474affc
DJDeka <dipjyoti20@gmail.com>
2023-07-08 11:53:22 +0530

    Usestate_2nd_program array of objects

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import GetData from "./GetData";
+
 function App() {
   return (
     <div className="App">
       <h1>My new FSD Journey</h1>
+      <GetData/>
     </div>
   );
 }
---
+++ src/datas.js
@@ -0,0 +1,14 @@
+const datas = [
+    {
+      id: 1,
+      name: 'Nick',
+      age: 21
+    },
+    {
+      id: 2,
+      name: 'Lara',
+      age: 30
+    }
+  ];
+
+  export default datas;
